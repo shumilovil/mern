c40e476f0d95e9add3b2ac75210b797859e09536
Ilya Shumilov <shumilovil@gmail.com>
2021-02-22 23:09:25 +0300

    Add redirect

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const fs = require('fs')
 const app = express()
 
 app.use(cors())
+app.use ((req, res, next) => {
+    console.log('secure', secure)
+    if (req.secure) {
+            // request was via https, so do no special handling
+            next();
+    } else {
+            // request was via http, so redirect to https
+            res.redirect('https://' + req.headers.host + req.url);
+    }
+});
 app.use(express.json({ extended: true }))
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/link', require('./routes/link.routes'))
